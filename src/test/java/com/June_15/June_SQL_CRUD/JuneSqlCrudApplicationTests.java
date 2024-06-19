bc52de9f060d89cdd5622336b25fe3d89dfeecdc
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-06-19 09:35:29 +0530

    Create JuneSqlCrudApplicationTests.java

---
+++ src/test/java/com/June_15/June_SQL_CRUD/JuneSqlCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.June_15.June_SQL_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuneSqlCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
