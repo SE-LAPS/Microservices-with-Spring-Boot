f81cf8d6c6d574e1ce235a9b253d983460f6149b
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-06-19 09:35:43 +0530

    Create JuneSqlCrudApplication.java

---
+++ src/main/java/com/June_15/June_SQL_CRUD/JuneSqlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.June_15.June_SQL_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuneSqlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuneSqlCrudApplication.class, args);
+	}
+
+}
